b9949e1c44e9a86f690030d39685b8eb73a4ab49
StormyWaters2021 <thomas.j.alsteen@gmail.com>
2026-07-23 01:00:30 -0700

    Renamed several icons

--- settings/icon-options.js
+++ settings/icon-options.js
@@ -17,7 +17,7 @@ export const STAT_DEFINITIONS = [
     defaultIcon: "fist",
     icons: [
       { id: "fist", label: "Attack", image: "assets/icons/attack/fist_white.png" },
-      { id: "autonomous", label: "Attack (Right)", image: "assets/icons/attack/fist_right_white.png" }
+      { id: "autonomous", label: "Autonomous", image: "assets/icons/attack/autonomous_white.png" }
     ]
   },
   {
@@ -26,8 +26,8 @@ export const STAT_DEFINITIONS = [
     defaultValue: 17,
     defaultIcon: "shield",
     icons: [
-      { id: "shield", label: "Shield", image: "assets/icons/defense/shield_white.png" },
-      { id: "circle", label: "Circle", image: "assets/icons/defense/circle_white.png" }
+      { id: "shield", label: "Defense", image: "assets/icons/defense/shield_white.png" },
+      { id: "circle", label: "Vehicle", image: "assets/icons/defense/vehicle_white.png" }
     ]
   },
   {
@@ -36,10 +36,10 @@ export const STAT_DEFINITIONS = [
     defaultValue: 2,
     defaultIcon: "burst",
     icons: [
-      { id: "burst", label: "Burst", image: "assets/icons/damage/burst_white.png" },
-      { id: "atom", label: "Atom", image: "assets/icons/damage/atom_white.png" },
+      { id: "burst", label: "Standard", image: "assets/icons/damage/standard_white.png" },
+      { id: "atom", label: "Tiny", image: "assets/icons/damage/tiny_white.png" },
       { id: "giant", label: "Giant", image: "assets/icons/damage/giant_white.png" },
-      { id: "fist", label: "Fist", image: "assets/icons/damage/fist_white.png", scale: 1.35 }
+      { id: "fist", label: "Colossal", image: "assets/icons/damage/colossal_white.png", scale: 1.35 }
     ]
   }
 ];
