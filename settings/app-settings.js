065a1a03d69e0c6195dbef891e18c03c180dfe7d
StormyWaters2021 <thomas.j.alsteen@gmail.com>
2026-08-02 14:04:10 -0700

    Added blank token for printing.

--- settings/app-settings.js
+++ settings/app-settings.js
@@ -2,7 +2,7 @@ export const APP_SETTINGS = {
   projectVersion: 4,
   versionmsg: {
     visible: true,
-    text: "HeroClix Bystander Generator v0.5.1"
+    text: "HeroClix Bystander Generator v0.5.2"
   },
   storageKey: "custom-token-maker-project-v1",
   themeStorageKey: "custom-token-maker-theme",
